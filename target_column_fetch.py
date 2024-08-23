fcd033525c9662e3f56f36405004b1bdf029b699
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-23 12:11:16 +0530

    Update target_column_fetch.py

--- target_column_fetch.py
+++ target_column_fetch.py
@@ -1,26 +1,26 @@
-import pandas as pd
+# import pandas as pd
 
-# Module Import
-import config
+# # Module Import
+# import config
 
 
-"""Get user input on target variable from the dataset"""
-def select_target_column_from_dataset(dataset : pd.DataFrame) -> str:
-    """Target col map
+# """Get user input on target variable from the dataset"""
+# def select_target_column_from_dataset(dataset : pd.DataFrame) -> str:
+#     """Target col map
 
-        Get target col name form user
+#         Get target col name form user
         
-        Parameters
-        ----------
-        dataset : pandas.DataFrame
+#         Parameters
+#         ----------
+#         dataset : pandas.DataFrame
     
-        Returns
-        -------
-        target_column : str
+#         Returns
+#         -------
+#         target_column : str
     
-        """
+#         """
 
-    target_name = input("Please enter the target column you want to select from the dataset: ")
-    print(f"You have selected: {target_name}")
-    return target_name
+#     target_name = input("Please enter the target column you want to select from the dataset: ")
+#     print(f"You have selected: {target_name}")
+#     return target_name
 
