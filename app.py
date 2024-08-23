6592a8be634a5188e05a7e3fd545b546702f79ab
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-23 13:30:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,11 +31,38 @@ import shap
 
 app = FastAPI()
 
+# Define the custom theme 
+custom_theme = gr.themes.Default(primary_hue= gr.themes.colors.green)
+# Define custom CSS and theme
+custom_css = """
+footer {visibility: hidden;}
+.gradio-container .image {
+        width: 10%;
+        height: 10%;
+        object-fit: contain;}
+
+"""
+def pycaret_process(file_path):
+    dataset = pd.read_csv(file_path)
+    target_column = "SalePrice"
+    r = RegressionExperiment()
+    filtered_features = dataset.loc[:, dataset.columns != target_column]
+    filtered_dataset = pd.DataFrame(filtered_features, columns=filtered_features.columns)
+    clf = r.setup(filtered_dataset, target=dataset[target_column],preprocess=False,transformation= False, session_id=2, experiment_name ='Regression',index=False)
+    best_model = clf.compare_models(fold=5,exclude = ['lightgbm','qda','dummy','ridge','catboost','br','et','lasso','huber','llar'])
+    return gr.Textbox(label="Best Model",value=best_model,placeholder="NOT SET")
+
 with gr.Blocks(analytics_enabled=False,css=custom_css,theme=custom_theme,title="SE FSTool") as FSTool:
-        pass
+    gr.Markdown("<span style='color: black;'>Please upload a CSV file</span>",sanitize_html=True)
+       
+    with gr.Row():
+        with gr.Column():  
+            file_input = gr.File(label="Upload Dataset",file_types=['.csv'],file_count="single")  
+            apply_button = gr.Button("Start Process",variant='primary',visible=True)
+            best_model = gr.Textbox(label="Best Model",placeholder="Best Model")
+            apply_button.click(pycaret_process,inputs=[file_input],outputs=[best_model])
 
 app = gr.mount_gradio_app(app, FSTool, path="/")
-
 
 # # Main Method
 # #def main():
