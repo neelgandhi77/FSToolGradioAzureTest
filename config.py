5eabbf3eeca179b56ffed3926466776bcd0f6a01
Neelkumar Gandhi <63502709+neelgandhi77@users.noreply.github.com>
2024-08-23 12:08:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import pandas as pd
-import json
-import os
+# import pandas as pd
+# import json
+# import os
 
-# Global Variables - Dont Delete any of this irrespective of Used/Unused
-ts_col_name = []
-column_names_dropdown = []
-col_name_map_dict = dict() # Not in use
-problem_type = ''
-filtered_dataset = pd.DataFrame() # Not in use
-target_column = ''
-best_model=''
-reduced_batches = []
+# # Global Variables - Dont Delete any of this irrespective of Used/Unused
+# ts_col_name = []
+# column_names_dropdown = []
+# col_name_map_dict = dict() # Not in use
+# problem_type = ''
+# filtered_dataset = pd.DataFrame() # Not in use
+# target_column = ''
+# best_model=''
+# reduced_batches = []
